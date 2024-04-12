b9a407cf9d2c7ae66d55473925bd7b186b77b22a
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-04-12 09:04:10 +0200

    Set relative path

--- scripts/fetch-abis.sh
+++ scripts/fetch-abis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
 # TODO: Get contracts ABIs from npm
-IN=$HOME/iexecdev/iexec-voucher-contracts/artifacts/contracts
+IN=../../iexec-voucher-contracts/artifacts/contracts
 OUT=../abis
 mkdir -p $OUT
 cat $IN/VoucherHub.sol/VoucherHub.json | jq .abi >$OUT/VoucherHub.json
