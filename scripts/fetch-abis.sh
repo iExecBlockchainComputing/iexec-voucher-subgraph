3df25ca6de4f9347d709520adc7568bd08dcf3a2
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-04-17 09:54:45 +0200

    Add voucher events to subgraph

--- scripts/fetch-abis.sh
+++ scripts/fetch-abis.sh
@@ -5,3 +5,4 @@ IN=../../iexec-voucher-contracts/artifacts/contracts
 OUT=../abis
 mkdir -p $OUT
 cat $IN/VoucherHub.sol/VoucherHub.json | jq .abi >$OUT/VoucherHub.json
+cat $IN/beacon/Voucher.sol/Voucher.json | jq .abi >$OUT/Voucher.json
