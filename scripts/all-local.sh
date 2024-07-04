a3c75e0223f24a7cc6df813f70153b9f98017a7e
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-07-04 17:14:19 +0200

    add jenkins deployement and c13 network rpc naming

--- scripts/all-local.sh
+++ scripts/all-local.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-npm run schema
+npm run clean
 npm run codegen
 npm run build
 docker-compose up -d
 sleep 10
-npm run create-local
-npm run deploy-local
+npm run create
+npm run deploy
