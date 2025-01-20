baa22099dd607453eb43c9a154fd373a596303ec
gfournieriExec <gabriel.fournier@iex.ec>
2025-01-20 17:40:22 +0100

    remove EventParam

--- tests/unit/utils/EventParamBuilder.ts
+++ tests/unit/utils/EventParamBuilder.ts
@@ -1,45 +1,36 @@
 import { Address, BigInt, Bytes, ethereum } from '@graphprotocol/graph-ts';
 
-export class EventParam {
-    constructor(
-        public key: string,
-        public value: ethereum.Value,
-    ) {}
-}
-
 export class EventParamBuilder {
-    private params: EventParam[] = [];
+    private params: ethereum.EventParam[] = new Array<ethereum.EventParam>();
 
     static init(): EventParamBuilder {
         return new EventParamBuilder();
     }
 
     address(key: string, value: Address): EventParamBuilder {
-        this.params.push(new EventParam(key, ethereum.Value.fromAddress(value)));
+        this.params.push(new ethereum.EventParam(key, ethereum.Value.fromAddress(value)));
         return this;
     }
 
     bytes(key: string, value: Bytes): EventParamBuilder {
-        this.params.push(new EventParam(key, ethereum.Value.fromBytes(value)));
+        this.params.push(new ethereum.EventParam(key, ethereum.Value.fromBytes(value)));
         return this;
     }
 
     bigInt(key: string, value: BigInt): EventParamBuilder {
-        this.params.push(new EventParam(key, ethereum.Value.fromUnsignedBigInt(value)));
+        this.params.push(new ethereum.EventParam(key, ethereum.Value.fromUnsignedBigInt(value)));
         return this;
     }
 
     string(key: string, value: string): EventParamBuilder {
-        this.params.push(new EventParam(key, ethereum.Value.fromString(value)));
+        this.params.push(new ethereum.EventParam(key, ethereum.Value.fromString(value)));
         return this;
     }
 
     build(event: ethereum.Event): void {
         event.parameters = new Array<ethereum.EventParam>();
         for (let i = 0; i < this.params.length; i++) {
-            event.parameters.push(
-                new ethereum.EventParam(this.params[i].key, this.params[i].value),
-            );
+            event.parameters.push(this.params[i]);
         }
     }
 }
