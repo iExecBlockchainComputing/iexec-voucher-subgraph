bd6e9376b651e82cb7f22968d389f15639589df2
gfournieriExec <gabriel.fournier@iex.ec>
2025-01-27 21:32:10 +0100

    update decimals to check that works

--- tests/unit/voucher/AccountAuthorized.test.ts
+++ tests/unit/voucher/AccountAuthorized.test.ts
@@ -15,8 +15,8 @@ const VOUCHER_DURATION = BigInt.fromI32(86400);
 const VOUCHER_TYPE_ELIGIBLE_ASSETS: string[] = [];
 
 const VOUCHER_OWNER = '0xabcdefabcdefabcdefabcdefabcdefabcdefabcd';
-const VOUCHER_VALUE = BigDecimal.fromString('100');
-const VOUCHER_BALANCE = BigDecimal.fromString('50');
+const VOUCHER_VALUE = BigDecimal.fromString('100.123');
+const VOUCHER_BALANCE = BigDecimal.fromString('50.456');
 const VOUCHER_EXPIRATION = BigInt.fromI32(999999);
 const VOUCHER_ADDRESS = '0x1234567890123456789012345678901234567890';
 
